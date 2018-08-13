e6dc025a7e8eb12eb4063ff81152e2cf7968b3ad
IdrisXort <Idris.Genc@eneco.com>
2018-08-13 16:21:40 +0200

    default proj

---
+++ LockedCustomerExtractor/LockedCustomerExtractor/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace LockedCustomerExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LockedCustomerFPLabel = new System.Windows.Forms.Label();
+            this.SourceFPLabel = new System.Windows.Forms.Label();
+            this.ResultFPLabel = new System.Windows.Forms.Label();
+            this.ExtractButton = new System.Windows.Forms.Button();
+            this.LockCustInput = new System.Windows.Forms.TextBox();
+            this.SourceInput = new System.Windows.Forms.TextBox();
+            this.ResultPathInput = new System.Windows.Forms.TextBox();
+            this.ReportFPInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LockedCustomerFPLabel
+            // 
+            this.LockedCustomerFPLabel.AutoSize = true;
+            this.LockedCustomerFPLabel.Location = new System.Drawing.Point(28, 32);
+            this.LockedCustomerFPLabel.Name = "LockedCustomerFPLabel";
+            this.LockedCustomerFPLabel.Size = new System.Drawing.Size(114, 13);
+            this.LockedCustomerFPLabel.TabIndex = 0;
+            this.LockedCustomerFPLabel.Text = "Lock customer filepath";
+            // 
+            // SourceFPLabel
+            // 
+            this.SourceFPLabel.AutoSize = true;
+            this.SourceFPLabel.Location = new System.Drawing.Point(31, 97);
+            this.SourceFPLabel.Name = "SourceFPLabel";
+            this.SourceFPLabel.Size = new System.Drawing.Size(78, 13);
+            this.SourceFPLabel.TabIndex = 1;
+            this.SourceFPLabel.Text = "Source filepath";
+            // 
+            // ResultFPLabel
+            // 
+            this.ResultFPLabel.AutoSize = true;
+            this.ResultFPLabel.Location = new System.Drawing.Point(33, 165);
+            this.ResultFPLabel.Name = "ResultFPLabel";
+            this.ResultFPLabel.Size = new System.Drawing.Size(74, 13);
+            this.ResultFPLabel.TabIndex = 2;
+            this.ResultFPLabel.Text = "Result filepath";
+            // 
+            // ExtractButton
+            // 
+            this.ExtractButton.Location = new System.Drawing.Point(604, 91);
+            this.ExtractButton.Name = "ExtractButton";
+            this.ExtractButton.Size = new System.Drawing.Size(136, 64);
+            this.ExtractButton.TabIndex = 3;
+            this.ExtractButton.Text = "Extract";
+            this.ExtractButton.UseVisualStyleBackColor = true;
+            this.ExtractButton.Click += new System.EventHandler(this.ExtractButton_Click);
+            // 
+            // LockCustInput
+            // 
+            this.LockCustInput.Location = new System.Drawing.Point(180, 29);
+            this.LockCustInput.Name = "LockCustInput";
+            this.LockCustInput.Size = new System.Drawing.Size(358, 20);
+            this.LockCustInput.TabIndex = 4;
+            // 
+            // SourceInput
+            // 
+            this.SourceInput.Location = new System.Drawing.Point(180, 94);
+            this.SourceInput.Name = "SourceInput";
+            this.SourceInput.Size = new System.Drawing.Size(358, 20);
+            this.SourceInput.TabIndex = 5;
+            // 
+            // ResultPathInput
+            // 
+            this.ResultPathInput.Location = new System.Drawing.Point(180, 162);
+            this.ResultPathInput.Name = "ResultPathInput";
+            this.ResultPathInput.Size = new System.Drawing.Size(358, 20);
+            this.ResultPathInput.TabIndex = 6;
+            // 
+            // ReportFPInput
+            // 
+            this.ReportFPInput.Location = new System.Drawing.Point(180, 229);
+            this.ReportFPInput.Name = "ReportFPInput";
+            this.ReportFPInput.Size = new System.Drawing.Size(358, 20);
+            this.ReportFPInput.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Report filepath";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 261);
+            this.Controls.Add(this.ReportFPInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResultPathInput);
+            this.Controls.Add(this.SourceInput);
+            this.Controls.Add(this.LockCustInput);
+            this.Controls.Add(this.ExtractButton);
+            this.Controls.Add(this.ResultFPLabel);
+            this.Controls.Add(this.SourceFPLabel);
+            this.Controls.Add(this.LockedCustomerFPLabel);
+            this.Name = "Form1";
+            this.Text = "Locked Customer Extractor v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LockedCustomerFPLabel;
+        private System.Windows.Forms.Label SourceFPLabel;
+        private System.Windows.Forms.Label ResultFPLabel;
+        private System.Windows.Forms.Button ExtractButton;
+        private System.Windows.Forms.TextBox LockCustInput;
+        private System.Windows.Forms.TextBox SourceInput;
+        private System.Windows.Forms.TextBox ResultPathInput;
+        private System.Windows.Forms.TextBox ReportFPInput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LockedCustomerExtractor/LockedCustomerExtractor/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace LockedCustomerExtractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void ExcecuteExtraction(string lockedCustomersPath, string sourcePath, string restultPath, string reportPath)
+        {
+            List<string> lockedCustomersIds = readLockedCustomers(lockedCustomersPath);
+            string startTime = DateTime.Now.ToString();
+            int nrOfLockedCustomers = lockedCustomersIds.Count();
+            if (nrOfLockedCustomers > 0)
+            {
+                List<string> matches = matchLockedCustomersToScource(lockedCustomersIds, sourcePath);
+                int nrOfMatches = matches.Count();
+                if (nrOfMatches > 0)
+                {
+                    var header = extractheader(sourcePath);
+                    Dictionary<string, bool> customIdsWithMatchingStatus = getCustomerIdWithMatchingStatus(lockedCustomersIds, matches);
+                    var printStatus = printResult(matches, header, restultPath);
+                    if(printStatus)
+                    {
+                        string endTime = DateTime.Now.ToString();
+                        var reportStatus = printReport(reportPath, startTime, endTime, nrOfLockedCustomers, nrOfMatches, customIdsWithMatchingStatus);
+                        MessageBox.Show("Printing Succesfull.");
+                    } else
+                    {
+                        MessageBox.Show("Printing Failed.");
+                    }
+                } else
+                {
+                    MessageBox.Show("No machtes found.");
+                }
+            } else
+            {
+                MessageBox.Show("No customers found in customerLock file");
+            }
+        }
+        private Dictionary<string, bool> getCustomerIdWithMatchingStatus(List<string> lockedCustomerIds, List<string> matches)
+        {
+            Dictionary<string, bool> customerIdsWithMatchingStatus = new Dictionary<string, bool>();
+            List<string> matchIds = new List<string>();
+            foreach(string mach in matches)
+            {
+                matchIds.Add(mach.Split(';')[0]);
+            }
+
+            foreach(string lockedCustomer in lockedCustomerIds)
+            {
+                if(matchIds.Contains(lockedCustomer))
+                {
+                    customerIdsWithMatchingStatus.Add(lockedCustomer, true);
+                } else
+                {
+                    customerIdsWithMatchingStatus.Add(lockedCustomer, false);
+                }
+            }
+            return customerIdsWithMatchingStatus;
+        }
+        private bool printReport(string reportPath, string startTime, string endTime, int nrOfLockedCustomers, int nrOfMatches, Dictionary<string, bool> customIdsWithMatchingStatus)
+        {
+            bool succes = false;
+            StreamWriter writer = new StreamWriter(reportPath);
+            try
+            {
+                writer.WriteLine("StartDateTime: {0}", startTime);
+                writer.WriteLine("EndDateTimeTime: {0}", endTime);
+                writer.WriteLine("Number of locked customers in LockedCustomeSource: {0}", nrOfLockedCustomers);
+                writer.WriteLine("Number of matches: {0}", nrOfMatches);
+                writer.WriteLine();
+                writer.WriteLine("Succesfully extracted customers:");
+                foreach (KeyValuePair<string, bool> customerwithStatus in customIdsWithMatchingStatus)
+                {
+                    if(customerwithStatus.Value)
+                    {
+                        writer.WriteLine(customerwithStatus.Key);
+                    }
+                }
+                writer.WriteLine();
+                writer.WriteLine("Customers not found in sourcefile:");
+                foreach (KeyValuePair<string, bool> customerwithStatus in customIdsWithMatchingStatus)
+                {
+                    if (!customerwithStatus.Value)
+                    {
+                        writer.WriteLine(customerwithStatus.Key);
+                    }
+                }
+
+                succes = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                writer.Close();
+            }
+            return succes;
+
+        }
+        private List <string> readLockedCustomers(string path)
+        {
+            List<string> lockedCustomersIds = new List<string>();
+            var reader = new StreamReader(path);
+            try
+            {
+                while(!reader.EndOfStream)
+                {
+                    lockedCustomersIds.Add(reader.ReadLine());
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                reader.Close();
+            }
+            return lockedCustomersIds;
+        }
+        private List<string> matchLockedCustomersToScource(List<string> lockedCustomerIds, string sourcePath)
+        {
+            List<string> matches = new List<string>();
+
+            var reader = new StreamReader(sourcePath);
+            try
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string customerNumber = line.Split(';')[0];
+                    if(lockedCustomerIds.Contains(customerNumber)) {
+                        matches.Add(line);
+                    }
+                }
+            } catch(Exception ex) {
+                MessageBox.Show(ex.ToString());
+            } finally
+            {
+                reader.Close();
+            }
+
+            return matches;
+        }
+        private string extractheader(string sourcePath)
+        {
+            var reader = new StreamReader(sourcePath);
+            var header = "";
+            try
+            {
+                header = reader.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                reader.Close();
+            }
+
+            return header;
+        }
+        private bool printResult(List<string> matches, string header, string resultPath)
+        {
+            bool succes = false;
+            StreamWriter writer = new StreamWriter(resultPath);
+            try
+            {
+                writer.WriteLine(header);
+                foreach(string match in matches)
+                {
+                    writer.WriteLine(match);
+                }
+                succes = true;
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            } finally
+            {
+                writer.Close();
+            }
+            return succes;
+        }
+        private void ExtractButton_Click(object sender, EventArgs e)
+        {
+            ExtractButton.Text = "Extracting data.... please wait";
+            ExtractButton.Enabled = false;
+
+            try
+            {
+                string lockedCustomerPath = null;
+                if (!String.IsNullOrEmpty(LockCustInput.Text)) {
+                    lockedCustomerPath = LockCustInput.Text;
+                } else
+                {
+                    throw new Exception("Please fill out a path to the file containing the locked customers to match.");
+                }
+                string sourcePath = null;
+                if (!String.IsNullOrEmpty(SourceInput.Text)) {
+                    sourcePath = SourceInput.Text;
+                } else
+                {
+                    throw new Exception("Please fill out a path to the importsource.");
+                }
+                string resultPath = null;
+                if (!String.IsNullOrEmpty(ResultPathInput.Text)) {
+                    resultPath = ResultPathInput.Text;
+                } else
+                {
+                    throw new Exception("Please fill out a path where the result will be saved.");
+                }
+                string reportPath = null;
+                if(!String.IsNullOrEmpty(ReportFPInput.Text))
+                {
+                    reportPath = ReportFPInput.Text;
+                } else
+                {
+                    throw new Exception("Please fill out a path where the report will be saved.");
+                }
+                ExcecuteExtraction(lockedCustomerPath, sourcePath, resultPath, reportPath);
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                ExtractButton.Enabled = true;
+                ExtractButton.Text = "Extract";
+            } finally
+            {
+                ExtractButton.Enabled = true;
+                ExtractButton.Text = "Extract";
+            }
+
+
+        }
+    }
+}
